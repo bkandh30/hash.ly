3fc48592f93df0ab01131f93d3dd9c1b6a90b0e1
bkandh30 <kandhari.bhavya20@gmail.com>
2025-09-03 19:40:19 -0700

    Updated next config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/api/links/**',
+      },
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_APP_URL?.replace('https://', '') || 'localhost',
+        pathname: '/api/links/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
